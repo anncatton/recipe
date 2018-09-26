cb1052753c9a5bc9c51b78b2f357018b3b91f4dc
Ann Catton <ann@eatsleepride.com>
2018-09-25 22:12:44 -0400

    squash all

---
+++ play2.js
@@ -0,0 +1,15 @@
+var foo = 1;
+var bar = 2;
+
+console.log(foo + bar);
+console.log(foo - bar);
+console.log('yo');
+console.log(foo);
+console.log(bar);
+
+var foo1 = 'one';
+var foo2 = 'two';
+var foo3 = [];
+
+foo3 = [...foo3, foo1, foo2];
+console.log('foo3:', foo3);
